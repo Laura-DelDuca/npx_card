51a37974ce0e8632cce33e22645befdf8c416384
Laura Del Duca <laura.delduca1990@gmail.com>
2019-01-23 15:11:19 +0100

    commit primordial: ajout du chat

--- index.js
+++ index.js
@@ -2,23 +2,29 @@
 
 //console.log('doing business');
 
-// Infos:
 // https://studioelsa.se/blog/open-source-oss-npx-business-card/
 
 const chalk = require ('chalk');
 const boxen = require ('boxen');
+const figlet = require ('figlet');
 
 const frame = {
     borderColor: 'green',
     borderStyle: 'double',
     margin: {
-        top: 4,
-        bottom: 4,
-        left: 8,
-        right: 8
+        top: 0,
+        bottom: 0,
+        left: 4,
+        right: 4
     },
     padding: 1
 };
+
+console.log(chalk.green.bold( figlet.textSync('                About Me:', {
+    font: 'Standard',
+    horizontalLayout: 'fitted',
+    verticalLayout: 'fitted',
+})));
 
 const card = '\n' +
     chalk.green ('                         Laura') + ' ' + chalk.green.bold('Del Duca') + '\n\n' +
@@ -29,10 +35,27 @@ const card = '\n' +
     chalk.yellow.bold('==> npm :') + ' ' + '       https://www.npmjs.com/' + chalk.yellow.italic('~lauradelduca1990') + '\n' +
     chalk.red.bold('==> Linkedin') + ' ' + '    https://www.linkedin.com/in/' + chalk.red.italic('laura-del-duca-887367172/') + '\n'
 
-
-
-
 console.log(boxen(card, frame));
+
+const cat1 = "  /\\___/\\" + '\n';
+const cat2 = "(  o o  )" + '\n';
+const cat3 = "/   *   \\ " + '\n';
+const cat4 = "\\__\\_/__/ meow!" + '\n';
+const cat5 = "  /   \\ " + '\n';
+const cat6 = " / ___ \\ " + '\n';
+const cat7 = " \\/___\\/ " + '\n'; 
+
+console.log(cat1, cat2, cat3, cat4, cat5, cat6, cat7);
+
+
+//http://www.ascii-fr.com/-Chats-.html
+//     /\___/\
+//    (  o o  )
+//    /   *   \
+//    \__\_/__/ meow!
+//      /   \
+//     / ___ \
+//     \/___\/
 
 
 
